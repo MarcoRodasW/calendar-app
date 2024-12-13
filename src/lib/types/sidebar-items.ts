436ec57635621bf748630d52244c4759e5d816a1
MarcoRodasW <mrodasw@gmail.com>
2024-12-13 12:31:58 -0600

    feat: set up dashboard layout

---
+++ src/lib/types/sidebar-items.ts
@@ -0,0 +1,20 @@
+import { Calendar, Home, List } from "lucide-react";
+
+// Menu items.
+export const SideBarItems = [
+    {
+      title: "Dahsboard",
+      url: "/dashboard",
+      icon: Home,
+    },
+    {
+      title: "Calendar",
+      url: "/dashboard/calendar",
+      icon: Calendar,
+    },
+    {
+      title: "Appointments",
+      url: "/dashboard/appointments",
+      icon: List,
+    },
+  ];
